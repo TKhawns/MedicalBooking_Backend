52c98d616f7aeb450c4bcb8d47cfea8c6fbaa4af
TKSkygod <trinhkhanhctdt@gmail.com>
2024-01-03 03:21:33 +0700

    config project

--- src/route/web.js
+++ src/route/web.js
@@ -1,9 +1,8 @@
 import express from 'express';
+import homeController from '../controllers/homeController';
 let router = express.Router();
 let initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send('Hello World');
-    });
+    router.get('/', homeController.getHomePage);
     return app.use('/', router);
 };
 module.exports = initWebRoutes;
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import viewEngine from './config/viewEngine';
 import initWebRoutes from './route/web';
+import testConnectDb from './config/connectDB';
 require('dotenv').config();
 let app = express();
 
@@ -11,6 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
+
+testConnectDb();
 
 let port = process.env.PORT || 8081;
 app.listen(port, () => {
